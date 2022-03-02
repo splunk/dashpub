0d9f9c7aa1dcd121e9c2358da24a31bbe94e7eba
Will Searle <will@livehybrid.com>
2022-03-02 14:30:44 +0000

    feaure: Dashboard selection from specific app

--- cli/assets.js
+++ cli/assets.js
@@ -82,7 +82,7 @@ async function storeImage(data, mimeType, { name = 'img', projectDir }) {
     return filename;
 }
 
-async function downloadImage(src, assetType, app, splunkdInfo, projectDir) {
+async function downloadImage(src, assetType, splunkdInfo, projectDir) {
     if (!src) {
         return src;
     }
@@ -106,9 +106,7 @@ async function downloadImage(src, assetType, app, splunkdInfo, projectDir) {
     if (type === 'splunk-enterprise-kvstore') {
         const imgData = await splunkd(
             'GET',
-            `/servicesNS/nobody/${encodeURIComponent(app)}/storage/collections/data/splunk-dashboard-${assetType}/${encodeURIComponent(
-                id
-            )}`,
+            `/servicesNS/nobody/${encodeURIComponent(constants.SPLUNK_DASHBOARDS_APP)/storage/collections/data/splunk-dashboard-${assetType}/${encodeURIComponent(id)}`,
             splunkdInfo
         );
 
